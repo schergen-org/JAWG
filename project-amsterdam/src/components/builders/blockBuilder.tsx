2fe3f7e831dc7c2cdc8c73eaf389eda006f7caf4
wrth1337 <benjamin.wirth98@gmail.com>
2025-04-14 13:58:34 +0200

    Linting

--- project-amsterdam/src/components/builders/blockBuilder.tsx
+++ project-amsterdam/src/components/builders/blockBuilder.tsx
@@ -2,28 +2,30 @@ import { ContentBlock, contentType } from '../../types/Schema';
 import Text from '../blocks/text/text'
 
 type blockBuilderProps = {
-    blocks: ContentBlock[];
+  blocks: ContentBlock[];
 }
 export default function BlockBuilder({ blocks }: blockBuilderProps) {
-    return (
-        <>
-            {
-                blocks.map((block) => {
-                    return (<div
-                        key={block.title}
-                    >
-                        <h2>
-                            {block.title}
-                        </h2>
-                        {block.blockType === 'Text' &&
-                            <Text 
-                                content={block.content as contentType['Text']}                          
-                            />
-                        }
-                    </div>)
-                })
-            }
+  return (
+    <>
+      {
+        blocks.map((block) => {
+          return (
+            <div
+              key={block.title}
+            >
+              <h2>
+                {block.title}
+              </h2>
+              {block.blockType === 'Text' &&
+                <Text
+                  content={block.content as contentType['Text']}
+                />
+              }
+            </div>
+          )
+        })
+      }
 
-        </>
-    )
+    </>
+  )
 }
