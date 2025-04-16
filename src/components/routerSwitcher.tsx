127bccc221a20ffd0f838b5df9f26c210c6421ac
Jonathan Rech <joni.w.rech@gmail.com>
2025-04-16 12:55:55 +0200

    Add navlinks to switch between pages

--- src/components/routerSwitcher.tsx
+++ src/components/routerSwitcher.tsx
@@ -1,32 +1,44 @@
 import { Route, Routes } from 'react-router';
 import { Page } from '../types/Schema';
 import BlockBuilder from './builders/blockBuilder';
+import { NavLink } from '@mantine/core';
 
 type routeSwitcherProps = {
   pages: Page[];
 }
 export  default function RouterSwitcher({ pages }: routeSwitcherProps) {
     return (
-        <Routes>
-            <Route path="/" element={
-                (pages.length > 0) && 
-            <BlockBuilder
-                blocks={pages[0].content}
-            />
-            }/>
+        <>
             {pages.map((page, index) => {
                 return (
-                    <Route 
+                    <NavLink
                         key={index}
-                        path={page.route} 
-                        element={
-                            <BlockBuilder
-                                blocks={page.content}
-                            />
-                        }
+                        label={page.title}
+                        href={"/" + page.route}
                     />
                 )
             })}
-        </Routes>
+            <Routes>
+                <Route path="/" element={
+                    (pages.length > 0) && 
+                <BlockBuilder
+                    blocks={pages[0].content}
+                />
+                }/>
+                {pages.map((page, index) => {
+                    return (
+                        <Route 
+                            key={index}
+                            path={page.route} 
+                            element={
+                                <BlockBuilder
+                                    blocks={page.content}
+                                />
+                            }
+                        />
+                    )
+                })}
+            </Routes>
+        </>
     )
 }
