1dadf3b85692c5a1e240442e86eae5a2ad4e593a
Jonathan Rech <joni.w.rech@gmail.com>
2025-04-16 12:43:25 +0200

    Add react router dependency

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { MantineProvider } from '@mantine/core'
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <MantineProvider>
-            <App />
-        </MantineProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </MantineProvider>      
     </StrictMode>,
 )
