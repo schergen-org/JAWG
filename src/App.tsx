c8a8be17036e2764707cd920b2d6c2c6a98daaef
wrth1337 <benjamin.wirth98@gmail.com>
2025-04-22 08:41:46 +0200

    Add AppShell to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,24 @@ import './App.css'
 import { Schema } from './types/Schema'
 import { loadJsonData } from './services/loadJsonService'
 import RouterSwitcher from './components/routerSwitcher';
+import { AppShell } from '@mantine/core';
 
 function App() {
-    const [data, setData] = useState<Schema | null>(null);
+  const [data, setData] = useState<Schema | null>(null);
 
-    useEffect(() => {
-        loadJsonData().then(setData)
-    }, []);
+  useEffect(() => {
+    loadJsonData().then(setData)
+  }, []);
 
-    return (
-        <>
-            <div>
-                <RouterSwitcher
-                    pages={data?.pages ?? []}
-                />
-                {/* <h1>JSON Viewer</h1>
-                <pre>{data ? JSON.stringify(data, null, 2) : 'Keine Daten verfügbar'}</pre> */}
-            </div>
-        </>
-    )
+  return (
+    <AppShell>
+      <AppShell.Main>
+        <RouterSwitcher
+          pages={data?.pages ?? []}
+        />
+      </AppShell.Main>
+    </AppShell>
+  )
 }
 
 export default App
