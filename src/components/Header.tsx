c464f39ed7df4efb81b3c1e91b2f87adfaa980df
wrth1337 <benjamin.wirth98@gmail.com>
2025-04-22 10:27:46 +0200

    Linting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
-import { NavLink } from "@mantine/core";
-import { Page } from "../types/Schema";
+import { NavLink } from '@mantine/core';
+import { Page } from '../types/Schema';
 
 type headerProps = {
   pages: Page[];
 }
 
 export default function Header({ pages }: headerProps) {
-  return (
-    pages.map((page, index) => {
-        return (
-            <NavLink
-                key={index}
-                label={page.title}
-                href={'/' + page.route}
-            />
-        )
-    })
-  )
+    return (
+        pages.map((page, index) => {
+            return (
+                <NavLink
+                    key={index}
+                    label={page.title}
+                    href={'/' + page.route}
+                />
+            )
+        })
+    )
 }
