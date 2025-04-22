f0ab43c0cf46c16889fd616ac3f93fc81599dd4d
wrth1337 <benjamin.wirth98@gmail.com>
2025-04-22 08:42:08 +0200

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import RouterSwitcher from './components/routerSwitcher';
 import { AppShell } from '@mantine/core';
 
 function App() {
-  const [data, setData] = useState<Schema | null>(null);
+    const [data, setData] = useState<Schema | null>(null);
 
-  useEffect(() => {
-    loadJsonData().then(setData)
-  }, []);
+    useEffect(() => {
+        loadJsonData().then(setData)
+    }, []);
 
-  return (
-    <AppShell>
-      <AppShell.Main>
-        <RouterSwitcher
-          pages={data?.pages ?? []}
-        />
-      </AppShell.Main>
-    </AppShell>
-  )
+    return (
+        <AppShell>
+            <AppShell.Main>
+                <RouterSwitcher
+                    pages={data?.pages ?? []}
+                />
+            </AppShell.Main>
+        </AppShell>
+    )
 }
 
 export default App
